96d2f83184d2a0d73be50e01481906ec09c3bee9
Badhon58 <badhon@theclinicall.com>
2025-12-07 12:47:39 +0600

    module added for nest.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import {
+  PrometheusModule,
+  makeCounterProvider,
+} from '@willsoto/nestjs-prometheus';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrometheusInterceptor } from './interceptors/prometheus.interceptor';
 
 @Module({
   imports: [PrometheusModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: PrometheusInterceptor,
+    },
+  ],
 })
 export class AppModule {}
