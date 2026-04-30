7e5dadfafc7ddf7acabff7acb43f8b1bb1e1287b
Badhon112 <biswasbadhon896@gmail.com>
2026-05-01 00:26:50 +0600

    Backend Done

--- backend/user/src/main.ts
+++ backend/user/src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const env = process.env.ENVIRONMENT;
+
+  const NATS_URL =
+    env === 'LOCAL' ? process.env.NATS_URL_LOCAL : process.env.NATS_URL_LIVE;
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.NATS,
       options: {
-        servers: ['nats://localhost:4222'],
+        servers: [NATS_URL!],
       },
     },
   );
