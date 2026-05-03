c59d2abd7f2b222033f2c68df9d6bb1bb3235d53
Badhon58 <badhon@theclinicall.com>
2026-05-03 12:06:41 +0600

    docker Successfully Running

--- backend/gateway/src/nats-client/nats-client-module.ts
+++ backend/gateway/src/nats-client/nats-client-module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
+const env = process.env.ENVIRONMENT;
+
+const NATS_URL =
+  env === 'LOCAL' ? process.env.NATS_URL_LOCAL : process.env.NATS_URL_LIVE;
+
 @Module({
   imports: [
     ClientsModule.register([
@@ -8,7 +13,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         name: 'NATS_SERVICE',
         transport: Transport.NATS,
         options: {
-          servers: ['nats://localhost:4222'],
+          servers: [NATS_URL!],
         },
       },
     ]),
